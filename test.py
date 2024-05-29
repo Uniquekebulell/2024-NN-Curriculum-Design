cbabbe7c635e5f0876d4c715169e200edad747ca
Uniquekebulell <doublej20032021@163.com>
2024-05-29 09:41:31 +0800

    v1

---
+++ test.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3], [10, 20, 30])
+plt.show()
+
+# import torch
+# print(torch.__version__)
+
+# # 使用range
+# for i in range(1,5,2):
+#     print(i)  # 输出 0, 1, 2, 3, 4
+
+# # 使用arange
+# import numpy as np
+# arr = np.arange(5)
+# print(arr)  # 输出 array([0, 1, 2, 3, 4])
